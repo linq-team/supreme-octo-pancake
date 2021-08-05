b8423130fe6d47b12452ae34a1f5800ab4eeb8dd
connoreggleston <connoreggleston10@gmail.com>
2021-08-04 19:20:02 -0700

    Add create react app skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Your code starts here */}
+      
+    </div>
+  );
+}
+
+export default App;
