c3b0071bed70770c93f71455d5580a6035f85ed9
royalflushever <royalflushdiamond918@gmail.com>
2022-02-21 11:33:50 -0600

    react component test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const baseURL = "https://fakerapi.it/api/v1/companies";
+
 function App() {
+  const [companies, setCompanies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCompanies();
+  }, []);
+
+  const getCompanies = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(baseURL);
+      const json = await response.json();
+      setCompanies(json.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const renderCompany = () => {
+    return (
+      <div>
+        <button>Refresh</button>
+        <table>
+          <thead>
+            <tr>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            { companies.map( (company) => {
+              return (
+                <tr key={company.id}>
+                  <td>{ company.name }</td>
+                  <td>{ company.contact.firstname + " " +  company.contact.lastname }</td>
+                  <td>{ company.country }</td>
+                  <td><a href={company.website}>{ company.website }</a></td>
+                </tr>
+              )
+            } ) }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div className="app">
       {/* Your code starts here */}
-      
+      <div>
+        { loading ? 'Loading...' : renderCompany()}
+      </div>
     </div>
   );
 }
