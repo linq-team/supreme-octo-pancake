0bac9c03c0b41810680c8b1391a986ed065a381c
willie <willierivera0815@gmail.com>
2022-04-13 02:41:40 +0800

    feature/LoadCompanies: Load companies from the fakerapi.it API with loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const baseUrl = "https://fakerapi.it/api/v1/companies";
+  const [companies, setCompanies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fields = ["name", "contact", "country", "website"];
+
+  const fetchCompanies = () => {
+    setLoading(true);
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((response) => {
+        setCompanies(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
   return (
     <div className="app">
-      {/* Your code starts here */}
-      
+      <button className="btn refresh" onClick={fetchCompanies}>
+        Refresh
+      </button>
+      {loading && <div className="loader"></div>}
+      {!loading && (
+        <table>
+          <thead>
+            {fields.map((field) => (
+              <th key={field}>
+                {field.charAt(0).toUpperCase() + field.slice(1)}
+              </th>
+            ))}
+          </thead>
+          <tbody>
+            {companies.map((company) => (
+              <tr>
+                {fields.map((field) => (
+                  <td>
+                    {field !== "contact"
+                      ? company[field]
+                      : company[field]["firstname"] +
+                        " " +
+                        company[field]["lastname"]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
