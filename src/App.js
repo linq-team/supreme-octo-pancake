a8927ea8220c1f2dc121debf66aaea2bf1e8161b
David Cheng <davidcheng1290@gmail.com>
2022-04-14 11:32:20 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react'
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react'
+import './App.css'
 
 function App() {
+  const [companies, setCompanies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleLoadCompanies = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await fetch(
+        'https://fakerapi.it/api/v1/companies'
+      )
+      const companies = await response.json()
+      setCompanies(companies.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    handleLoadCompanies()
+  }, [handleLoadCompanies])
+
+  if (isLoading) return <div>loading....</div>
+
   return (
     <div className="app">
-      {/* Your code starts here */}
-      
+      <button onClick={handleLoadCompanies}>Refresh</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>website</th>
+          </tr>
+        </thead>
+        {companies?.map((company, index) => (
+          <tbody key={index}>
+            <tr
+              style={{
+                backgroundColor: index % 2 ? 'black' : 'gray',
+              }}
+            >
+              <td>{company.name}</td>
+              <td>{`${company.contact.firstname} ${company.contact.lastname}`}</td>
+              <td>{company.country}</td>
+              <td>
+                <a href={company.website}>{company.website}</a>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
